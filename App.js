e8524c93849b2fc1646a19427969d2f613558018
Jennifer Spicer <xJenniferSpicer@gmail.com>
2023-11-20 13:31:43 -0600

    Added HomeScreen, SignIn, and SignUp to route

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
+import SignUp from "./src/screens/SignUp";
+import SignIn from "./src/screens/SignIn";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    SignIn: SignIn,
+    SignUp: SignUp,
   },
   {
-    initialRouteName: "Home",
+    initialRouteName: "SignIn",
     defaultNavigationOptions: {
       title: "App",
     },
