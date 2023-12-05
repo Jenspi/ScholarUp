4ba0460cfabc01ac72a7b28c2e433e534267288d
Jumana Suleiman <148403239+JumanaCS@users.noreply.github.com>
2023-12-05 00:48:17 -0600

    Flashcard Screen

---
+++ src/screens/Flash.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const FlashCard = () => {
+  return <Text style={styles.text}>Jumana Suleiman</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default FlashCard;
